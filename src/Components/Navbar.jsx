b6666439cb2057e14fb18c5aa22915290028f42b
Anthony Rodrigues <Leonidous2@gmail.com>
2023-03-06 14:59:04 -0500

    Navbar search button function added and search bar css styling fixed

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -26,16 +26,6 @@ const Search = styled('div')(({ theme }) => ({
   },
 }));
 
-const SearchIconWrapper = styled('div')(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-}));
-
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
   color: 'inherit',
   '& .MuiInputBase-input': {
@@ -44,50 +34,60 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
     paddingLeft: `calc(1em + ${theme.spacing(4)})`,
     transition: theme.transitions.create('width'),
     width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      width: '12ch',
-      '&:focus': {
-        width: '20ch',
-      },
-    },
   },
 }));
 
 export default function SearchAppBar() {
+
   return (
         <Box sx={{ flexGrow: 1 }}>
         <ThemeProvider theme={myTheme}>
           <AppBar position="static">
               <Toolbar>
-              <IconButton
-                  size="large"
-                  edge="start"
-                  color="inherit"
-                  aria-label="open drawer"
-                  sx={{ mr: 2 }}
-              >
-                  <MenuIcon />
-              </IconButton>
-              <Typography
-                  variant="h6"
-                  noWrap
-                  component="div"
-                  sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
-              >
-                  Pokédex
-              </Typography>
-              <Search>
-                  <SearchIconWrapper>
-                  <SearchIcon />
-                  </SearchIconWrapper>
+                <IconButton
+                    size="large"
+                    edge="start"
+                    color="inherit"
+                    aria-label="open drawer"
+                    sx={{ mr: 2 }}
+                >
+                    <MenuIcon />
+                </IconButton>
+                <Typography
+                    variant="h6"
+                    noWrap
+                    component="div"
+                    sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+                >
+                    Pokédex
+                </Typography>
+                <Search>
+                  <IconButton sx={{
+                    height: '100%',
+                    color: 'inherit',
+                    position: 'absolute',
+                    pointerEvents: 'auto',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    zIndex: '1',
+                  }}
+                  onClick={() => {console.log('hello');}}
+                  >
+                    <SearchIcon />
+                  </IconButton>
                   <StyledInputBase
-                  placeholder="Search…"
-                  inputProps={{ 'aria-label': 'search' }}
+                    placeholder="Search…"
+                    inputProps={{ 'aria-label': 'search' }}
+                    onChange={handleTextInputChange}
                   />
-              </Search>
+                </Search>
               </Toolbar>
           </AppBar>
         </ThemeProvider>
         </Box>
   );
+}
+
+function handleTextInputChange(){
+  console.log('text changed');
 }
