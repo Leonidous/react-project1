794b7bda37a92ba947cf61f36e4a62ee1fe4277a
Anthony Rodrigues <Leonidous2@gmail.com>
2023-03-20 17:25:27 -0400

    added type icons to pokeCard and stat numbers to table

--- src/Components/PokeCard.jsx
+++ src/Components/PokeCard.jsx
@@ -4,7 +4,6 @@ import useFetch from '../Hooks/Pokeapi';
 import fallbackimg from '../Images/FallbackImage.png'
 import PokeChart from './PokeStatChart';
 import Carousel from 'react-material-ui-carousel'
-import { Paper, Button } from '@mui/material'
 import '../App.css'
 
 export function PokeCard() {
@@ -20,15 +19,19 @@ export function PokeCard() {
     const pokeColors = ['#e4000f','#f08030','#f8d030','#6890f0','#78c850','#f85888'];
     const pokeBorderColors = ['#a60000','#9c531f','#a1871f','#445e9c','#4e8234','#a13959']
 
-    if(!(Object.keys(pokemoninfo).length==0)){
-        console.log(pokemoninfo.stats.map((statlist) => statlist.stat.name)+' ('+pokemoninfo.stats.map((statlist) => statlist.base_stat)+')');
+    let statLabels = [];
 
-        
+    if(!(Object.keys(pokemoninfo).length==0)){
+        for(let i in pokemoninfo.stats){
+            statLabels[i]=(pokemoninfo.stats[i].stat.name+' ('+pokemoninfo.stats[i].base_stat+')');
+        }
+        console.log(statLabels);
     }
+
 
     if(!(Object.keys(pokemoninfo).length == 0)){
         pokeStats = {
-            labels: (pokemoninfo.stats.map((statlist) => statlist.stat.name)),
+            labels: (statLabels.map((statLabel) => statLabel)),
             datasets: [
                 {
                 data: pokemoninfo.stats.map((statlist) => statlist.base_stat),
@@ -48,7 +51,7 @@ export function PokeCard() {
                 <h1 className='PokeCardTitle'>{pokemoninfo.name}</h1>
                 <div id='pokecard-grid'>
                     <div className='pokeGridItem'>
-                        <Carousel autoPlay={false}>
+                        <Carousel autoPlay={false} showArrows={true}>
                             <img src={'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/'+pokemoninfo.id+'.png'} onError={onMediaFallback} style={{marginLeft: "auto", marginRight: "auto", display: "flex", justifyContent: "center"}}></img>
                             <img src={'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/'+pokemoninfo.id+'.png'} onError={onMediaFallback} style={{marginLeft: "auto", marginRight: "auto", display: "flex", justifyContent: "center"}}></img>
                         </Carousel>
@@ -57,12 +60,12 @@ export function PokeCard() {
                         <PokeChart stats={pokeStats}/>
                     </div>
                     <div className='pokeGridItem'>
-                        <h3>Type(s)</h3>
-                        <ul>
-                            {pokemoninfo.types.map((typelist, key) => (
-                                <li key={key}>{typelist.type.name}</li>
+                        <h3 style={{textAlign: 'center'}}>Type(s)</h3>
+                        <div id='TypeList'>
+                            {pokemoninfo.types.map((typelist) => (
+                                <div className='pokeTypeImg'><img src={PokeType(typelist.type.name)}/><div className='pokeTypeName'>{typelist.type.name}</div></div>
                             ))}
-                        </ul>
+                        </div>
                     </div>
                     <div className='pokeGridItem'>
                         <h3>Abilities</h3>
@@ -82,10 +85,77 @@ export function PokeCard() {
         )
     }
 
+
+
+function PokeType(type){
+    console.log(type);
+    const images = importAll(require.context('../Images/typeIcons', false, /\.(png|jpe?g|svg)$/));
+
+    if(type==='grass'){
+        return(images['Grass_icon.png']);
+    }
+    if(type==='poison'){
+        return(images['Poison_icon.png']);
+    }
+    if(type==='bug'){
+        return(images['Bug_icon.png']);
+    }
+    if(type==='dark'){
+        return(images['Dark_icon.png']);
+    }
+    if(type==='dragon'){
+        return(images['Dragon_icon.png']);
+    }
+    if(type==='electric'){
+        return(images['Electric_icon.png']);
+    }
+    if(type==='fairy'){
+        return(images['Fairy_icon.png']);
+    }
+    if(type=='fighting'){
+        return(images['Fighting_icon.png']);
+    }
+    if(type==='fire'){
+        return(images['Fire_icon.png']);
+    }
+    if(type==='flying'){
+        return(images['Flying_icon.png']);
+    }
+    if(type==='ghost'){
+        return(images['Ghost_icon.png']);
+    }
+    if(type==='ground'){
+        return(images['Ground_icon.png']);
+    }
+    if(type==='ice'){
+        return(images['Ice_icon.png']);
+    }
+    if(type==='normal'){
+        return(images['Normal_icon.png']);
+    }
+    if(type==='psychic'){
+        return(images['Psychic_icon.png']);
+    }
+    if(type==='rock'){
+        return(images['Rock_icon.png']);
+    }
+    if(type==='steel'){
+        return(images['Steel_icon.png']);
+    }
+    if(type==='water'){
+        return(images['Water_icon.png']);
+    }
+}
+
+function importAll(r) {
+    let images = {};
+     r.keys().forEach((item, index) => { images[item.replace('./', '')] = r(item); });
+    return images
+   }
     /*What to add:
     -Pokemon Sprites (maybe be able to cycle through all of them?)
     -Graph of pokemon stats
     -Moves (in collapsible/scrollable list, will also show flavor text and what version learned in)
-    -
+    -Types with little type icons
     */
 }
