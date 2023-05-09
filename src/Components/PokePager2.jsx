4408b60a39e56c4c7d13d2ed7736fd75e8879f7a
Anthony Rodrigues <Leonidous2@gmail.com>
2023-05-09 13:59:54 -0400

    styled PokePager2, added bootstrap dropdown for sel poke per page

--- src/Components/PokePager2.jsx
+++ src/Components/PokePager2.jsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import Pagination from 'react-bootstrap/Pagination';
+import Container from 'react-bootstrap/Container';
+import Dropdown from 'react-bootstrap/Dropdown';
 import PokeContentGen2 from './PokeContentGen2';
 import { createBrowserHistory } from '@remix-run/router';
 import { useEffect } from 'react';
@@ -15,19 +17,15 @@ export default function PokePager() {
         return setPage(Number(event.target.text));
     };
 
-    const handleFirst = () => {
-        return setPage(1);
-    }
-
-    const handleLast = () => {
-        return setPage(pages);
-    }
-
     const handlePrev = () => {
         return setPage(page-1);
     }
     const handleNext = () => {
         return setPage(page+1);
+    }
+
+    const handlePerPage = (event) => {
+        return setperPage(Number(event.target.text));
     }
     
     //Preserve page state using browser history VVVVVVV
@@ -37,7 +35,6 @@ export default function PokePager() {
       const filterParams = history.location.search.substr(1);
       const filtersFromParams = qs.parse(filterParams);
       if (filtersFromParams.page && !(isNaN(filtersFromParams.page))) {
-        console.log(Number(filtersFromParams.page));
         setPage(Number(filtersFromParams.page));
       }
     }, []);
@@ -67,35 +64,9 @@ export default function PokePager() {
 
     let pageArray = [];
 
-    console.log(page);
-
     if(pageCheck.count){
 
-        /*
-        for(let pagenum=1; pagenum<=pages; pagenum++){
-
-            if(pagenum === page){
-                pageArray.push(
-                    <Pagination.Item key={pagenum} active={true} onClick={handleChange}>
-                        {pagenum}
-                    </Pagination.Item>,
-                )
-            }else{
-                pageArray.push(
-                    <Pagination.Item key={pagenum} active={false} onClick={handleChange}>
-                        {pagenum}
-                    </Pagination.Item>,
-                )
-            }
-        }
-        */
-
         pageArray.push(
-            <Pagination.First
-                key="first"
-                disabled={page === 1}
-                onClick={handleFirst}
-            />,
             <Pagination.Prev
             key="prev"
             disabled={page === 1}
@@ -104,17 +75,44 @@ export default function PokePager() {
         )
 
         if(page < pages-4){
-            for(let i = Math.max(1, page); i <= Math.min(pages, page + 4); i++){
-                pageArray.push(
-                    <Pagination.Item key={i} onClick={handleClick} active={page===i}>{i}</Pagination.Item>
-                )
-                console.log(i);
+
+            if(page==1){
+                for(let i = Math.max(1, page); i <= Math.min(pages, page + 4); i++){
+                    pageArray.push(
+                        <Pagination.Item key={i} onClick={handleClick} active={page===i}>{i}</Pagination.Item>
+                    )
+                }
             }
+            
+            if(page==2){
+                for(let i = page-1; i <= Math.min(pages, page + 3); i++){
+                    pageArray.push(
+                        <Pagination.Item key={i} onClick={handleClick} active={page===i}>{i}</Pagination.Item>
+                    )
+                }
+            }
+
+            if(page>2){
+                if(page>3){
+                    pageArray.push(
+                        <Pagination.Item key={1} onClick={handleClick}>{1}</Pagination.Item>,
+                        <Pagination.Ellipsis key="ellipsis-start" />
+                    )
+                }
+                for(let i = page-2; i <= Math.min(pages, page + 2); i++){
+                    pageArray.push(
+                        <Pagination.Item key={i} onClick={handleClick} active={page===i}>{i}</Pagination.Item>
+                    )
+                }
+            }
+
             pageArray.push(
-                <Pagination.Ellipsis key="ellipsis-end" />
+                <Pagination.Ellipsis key="ellipsis-end" />,
+                <Pagination.Item key={pages} onClick={handleClick}>{pages}</Pagination.Item>
             );
         }else{
             pageArray.push(
+                <Pagination.Item key={1} onClick={handleClick}>{1}</Pagination.Item>,
                 <Pagination.Ellipsis key="ellipsis-start" />
             );
             pageArray.push(
@@ -131,19 +129,27 @@ export default function PokePager() {
             key="next"
             disabled={page === pages}
             onClick={handleNext}
-            />,
-            <Pagination.Last
-                key="last"
-                disabled={page===pages}
-                onClick={handleLast}
             />
         )
-        return (
-            <>
-                <Pagination>
-                    {pageArray}
-                </Pagination>
 
+        return (
+            <>  
+                <Container className='pokepager-container'>
+                        <Pagination>
+                            {pageArray}
+                        </Pagination>
+                    <Dropdown>
+                        <Dropdown.Toggle variant="success" id="dropdown-basic">
+                            Select Pokémon Per Page ({perPage})
+                        </Dropdown.Toggle>
+
+                        <Dropdown.Menu>
+                            <Dropdown.Item onClick={handlePerPage}>24</Dropdown.Item>
+                            <Dropdown.Item onClick={handlePerPage}>48</Dropdown.Item>
+                            <Dropdown.Item onClick={handlePerPage}>72</Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
+                </Container>
                 <PokeContentGen2 page={page} pokePerPage={perPage}/>
             </>
         );
